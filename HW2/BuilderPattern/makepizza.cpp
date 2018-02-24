91192e2ba4ba6ab4ad28199043b2b73c6a12d327
kgrosul <34602954+kgrosul@users.noreply.github.com>
2018-02-24 15:48:47 +0300

    Create makepizza.cpp

---
+++ HW2/BuilderPattern/makepizza.cpp
@@ -0,0 +1,28 @@
+#include "makepizza.h"
+
+std::shared_ptr<CPizza> makeHawaiianPizza(){
+    CChef chef;
+    СHawaiianPizzaBuilder builder;
+    chef.setPizzaBuilder(&builder);
+    chef.createPizza();
+    auto pizza = chef.getPizza();
+    return pizza;
+}
+
+std::shared_ptr<CPizza> make4СheesesPizza(){
+    CChef chef;
+    С4СheesesPizzaBuilder builder;
+    chef.setPizzaBuilder(&builder);
+    chef.createPizza();
+    auto pizza = chef.getPizza();
+    return pizza;
+}
+
+std::shared_ptr<CPizza> makeMargaritaPizza(){
+    CChef chef;
+    СMargaritaPizzaBuilder builder;
+    chef.setPizzaBuilder(&builder);
+    chef.createPizza();
+    auto pizza = chef.getPizza();
+    return pizza;
+}
