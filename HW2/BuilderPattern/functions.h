9caeddd5d1fb86d9eeb138c1c496a8a1af0c1bf3
kgrosul <34602954+kgrosul@users.noreply.github.com>
2018-02-24 15:49:24 +0300

    Create functions.h

---
+++ HW2/BuilderPattern/functions.h
@@ -0,0 +1,12 @@
+#ifndef PIZZA_FUNCTIONS_H
+#define PIZZA_FUNCTIONS_H
+
+#include "makepizza.h"
+
+bool orders();
+void deliverOreder();
+std::string getOrder();
+void makeOrder();
+
+
+#endif //PIZZA_FUNCTIONS_H
