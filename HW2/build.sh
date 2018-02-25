745d6d456fec18f78474363dfc471eb9681843e5
kgrosul <34602954+kgrosul@users.noreply.github.com>
2018-02-25 21:35:58 +0300

    add google test

--- HW2/build.sh
+++ HW2/build.sh
@@ -1,1 +1,2 @@
-g++ -std=c++11 main.cpp run.cpp makepizza.cpp otherfood.cpp pizza.cpp
+#!/bin/bash
+g++ -std=c++11 main.cpp run.cpp makepizza.cpp otherfood.cpp pizza.cpp -lgtest -pthread -lgtest_main -L/usr/lib/
