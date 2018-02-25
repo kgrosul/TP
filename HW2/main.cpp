b901eb40bd840c8edad5bb42756642210128a43d
kgrosul <34602954+kgrosul@users.noreply.github.com>
2018-02-25 21:37:28 +0300

    Change for testing

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,7 +1,9 @@
-
 #include "run.h"
+#include "gtest/gtest.h"
+#include "tests.cpp"
 
-int main(){
-    run();
-    return 0;
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    //run(); - use to start
 }
