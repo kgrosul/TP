ace7c3a31b0b335feffc084b1c626e1b63fab4de
kgrosul <34602954+kgrosul@users.noreply.github.com>
2018-02-25 10:03:03 +0300

    Delete makepizza.h

--- HW2/BuilderPattern/makepizza.h
+++
@@ -1,6 +0,0 @@
-#include "pizza.h"
-
-std::shared_ptr<CPizza> makeHawaiianPizza();
-std::shared_ptr<CPizza> make4СheesesPizza();
-std::shared_ptr<CPizza> makeMargaritaPizza();
-std::shared_ptr<CPizza> makeCustomPizza();
